b65d95c6963c046cdff11924b054359216727599
Micheal Parks <micheal.parks@outlook.com>
2026-03-20 16:12:14 -0400

    widget

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,84 +1,126 @@
 export { default as ConnectionStatus } from './connection-status.svelte'
 
 export { default as ArmWidget } from './widgets/arm/arm.svelte'
-export { default as ArmMoveToJointPositionsWidget } from './widgets/arm/move-to-joint-positions-view.svelte'
-export { default as ArmMoveToPositionWidget } from './widgets/arm/move-to-position-view.svelte'
-export { default as ArmQuickMoveView } from './widgets/arm/quick-move-view.svelte'
+export { default as ArmMoveToJointPositionsWidget } from './widgets/arm/move-to-joint-positions-widget.svelte'
+export { default as ArmMoveToPositionWidget } from './widgets/arm/move-to-position-widget.svelte'
+export { default as ArmQuickMoveWidget } from './widgets/arm/quick-move-widget.svelte'
 
-export { default as BaseView } from './widgets/base/base.svelte'
-export { default as BaseMoveStraightView } from './widgets/base/move-straight-view.svelte'
-export { default as BaseQuickMoveView } from './widgets/base/quick-move-view.svelte'
-export { default as BaseSetPowerView } from './widgets/base/set-power-view.svelte'
-export { default as BaseSetVelocityView } from './widgets/base/set-velocity-view.svelte'
-export { default as BaseSpinView } from './widgets/base/spin-view.svelte'
+export { default as BaseWidget } from './widgets/base/base.svelte'
+export { default as BaseMoveStraightWidget } from './widgets/base/move-straight-widget.svelte'
+export { default as BaseQuickMoveWidget } from './widgets/base/quick-move-view.svelte'
+export { default as BaseSetPowerWidget } from './widgets/base/set-power-view.svelte'
+export { default as BaseSetVelocityWidget } from './widgets/base/set-velocity-view.svelte'
+export { default as BaseSpinWidget } from './widgets/base/spin-view.svelte'
 
-export { default as BoardView } from './widgets/board/board.svelte'
-export { default as BoardReadWritePinsView } from './widgets/board/read-write-pins-view.svelte'
+export { default as BoardWidget } from './widgets/board/board.svelte'
+export { default as BoardReadWritePinsWidget } from './widgets/board/read-write-pins-view.svelte'
 
-export { default as ButtonView } from './widgets/button/button.svelte'
-export { default as ButtonPushView } from './widgets/button/push.svelte'
+export { default as ButtonWidget } from './widgets/button/button.svelte'
+export { default as ButtonPushWidget } from './widgets/button/push.svelte'
 
-export { default as CameraView } from './widgets/camera/camera.svelte'
+export { default as CameraWidget } from './widgets/camera/camera.svelte'
 export { default as LiveOrPollingVideo } from './widgets/camera/live-or-polling-video.svelte'
 
 export type {
 	ComponentPreview,
 	ComponentPreviewSnippet,
 } from './widgets/discovery/component-preview.ts'
-export { default as DiscoveryView } from './widgets/discovery/discovery.svelte'
+export { default as DiscoveryWidget } from './widgets/discovery/discovery.svelte'
 
-export { default as DoCommandView } from './widgets/do-command/do-command.svelte'
+export { default as DoCommandWidget } from './widgets/do-command/do-command.svelte'
 
-export { default as EncoderView } from './widgets/encoder/encoder.svelte'
+export { default as EncoderWidget } from './widgets/encoder/encoder.svelte'
 
-export { default as GantryView } from './widgets/gantry/gantry.svelte'
-export { default as GantryHomeView } from './widgets/gantry/home.svelte'
-export { default as GantryMoveToPositionView } from './widgets/gantry/move-to-position.svelte'
-export { default as GantryQuickMoveView } from './widgets/gantry/quick-move.svelte'
+export { default as GantryWidget } from './widgets/gantry/gantry.svelte'
+export { default as GantryHomeWidget } from './widgets/gantry/home.svelte'
+export { default as GantryMoveToPositionWidget } from './widgets/gantry/move-to-position.svelte'
+export { default as GantryQuickMoveWidget } from './widgets/gantry/quick-move.svelte'
 
-export { default as GenericView } from './widgets/generic/generic.svelte'
+export { default as GenericWidget } from './widgets/generic/generic.svelte'
 
-export { default as GripperGrabView } from './widgets/gripper/grab.svelte'
-export { default as GripperView } from './widgets/gripper/gripper.svelte'
-export { default as GripperOpenView } from './widgets/gripper/open.svelte'
+export { default as GripperGrabWidget } from './widgets/gripper/grab.svelte'
+export { default as GripperWidget } from './widgets/gripper/gripper.svelte'
+export { default as GripperOpenWidget } from './widgets/gripper/open.svelte'
 
-export { default as InputControllerView } from './widgets/input-controller/input-controller.svelte'
+export { default as InputControllerWidget } from './widgets/input-controller/input-controller.svelte'
 
-export { default as MLModelServiceView } from './widgets/ml-model-service/ml-model-service.svelte'
+export { default as MLModelServiceWidget } from './widgets/ml-model-service/ml-model-service.svelte'
 
-export { default as MotorGoForView } from './widgets/motor/go-for-view.svelte'
-export { default as MotorGoToView } from './widgets/motor/go-to-view.svelte'
-export { default as MotorView } from './widgets/motor/motor.svelte'
-export { default as MotorQuickMoveView } from './widgets/motor/quick-move-view.svelte'
-export { default as MotorSetPowerView } from './widgets/motor/set-power-view.svelte'
-export { default as MotorSetRPMView } from './widgets/motor/set-rpm-view.svelte'
+export { default as MotorGoForWidget } from './widgets/motor/go-for-view.svelte'
+export { default as MotorGoToWidget } from './widgets/motor/go-to-view.svelte'
+export { default as MotorWidget } from './widgets/motor/motor.svelte'
+export { default as MotorQuickMoveWidget } from './widgets/motor/quick-move-view.svelte'
+export { default as MotorSetPowerWidget } from './widgets/motor/set-power-view.svelte'
+export { default as MotorSetRPMWidget } from './widgets/motor/set-rpm-view.svelte'
 
-export { default as MovementSensorView } from './widgets/movement-sensor/movement-sensor.svelte'
+export { default as MovementSensorWidget } from './widgets/movement-sensor/movement-sensor.svelte'
 
-export { default as NavigationServiceView } from './widgets/navigation/navigation.svelte'
+export { default as NavigationServiceWidget } from './widgets/navigation/navigation.svelte'
 
-export { default as PowerSensorView } from './widgets/power-sensor/power-sensor.svelte'
+export { default as PowerSensorWidget } from './widgets/power-sensor/power-sensor.svelte'
 
-export { default as SensorView } from './widgets/sensor/sensor.svelte'
+export { default as SensorWidget } from './widgets/sensor/sensor.svelte'
 
-export { default as ServoMoveView } from './widgets/servo/move-view.svelte'
-export { default as ServoQuickMoveView } from './widgets/servo/quick-move-view.svelte'
-export { default as ServoView } from './widgets/servo/servo.svelte'
+export { default as ServoMoveWidget } from './widgets/servo/move-view.svelte'
+export { default as ServoQuickMoveWidget } from './widgets/servo/quick-move-view.svelte'
+export { default as ServoWidget } from './widgets/servo/servo.svelte'
 
 export { default as SlamMap2D } from './slam/map2d/index.svelte'
-export { default as SlamView } from './widgets/slam/slam.svelte'
+export { default as SlamWidget } from './widgets/slam/slam.svelte'
 
-export { default as StopView } from './stop.svelte'
+export { default as StopWidget } from './stop.svelte'
 
-export { default as SwitchPositionView } from './widgets/switch/position-view.svelte'
-export { default as SwitchView } from './widgets/switch/switch.svelte'
+export { default as SwitchPositionWidget } from './widgets/switch/position-view.svelte'
+export { default as SwitchWidget } from './widgets/switch/switch.svelte'
 
 export { labelToColor } from './widgets/vision-service/color'
 export { getImageSize, type Size } from './widgets/vision-service/get-image-size'
-export { default as VisionServiceView } from './widgets/vision-service/vision-service.svelte'
+export { default as VisionServiceWidget } from './widgets/vision-service/vision-service.svelte'
 
 export { default as RefetchController } from './refetch-controller.svelte'
-export * from './navigation-map'
-export * from './maplibre'
+
+export {
+	NavigationMap,
+	NavigationTab,
+	type NavigationTabType,
+	type Shapes,
+	type CapsuleGeometry,
+	type SphereGeometry,
+	type BoxGeometry,
+	type Geometry,
+	type Obstacle,
+	type Path,
+} from './navigation-map'
+
+export {
+	MapLibre,
+	MapLibreMarker,
+	CenterControls,
+	FollowControls,
+	NavigationControls,
+	SatelliteControls,
+	DirectionalMarker,
+	LngLatInput,
+	MapProviders,
+	type MapProvider,
+	GeoPose,
+	Waypoint,
+	useMapLibre,
+	useMapLibreEvent,
+	useMapLibreThreeRaycast,
+	useMapLibreThreeRenderer,
+	lngLatToMercator,
+	mercatorToCartesian,
+	lngLatToCartesian,
+	cartesianToMercator,
+	cartesianToLngLat,
+} from './maplibre'
+
 export { default as BoundingBoxLabeler } from './image-annotations/bounding-box-labeler.svelte'
-export * from './image-annotations/labeler-constants'
+
+export {
+	ZOOM_MIN,
+	ZOOM_MAX,
+	INVERT_Y_FLOAT_DISTANCE,
+	INVERT_X_FLOAT_DISTANCE,
+} from './image-annotations/labeler-constants'
--- src/lib/components/navigation-map/index.ts
+++ src/lib/components/navigation-map/index.ts
@@ -1,2 +1,13 @@
 export { default as NavigationMap } from './index.svelte'
-export * from './types'
+
+export {
+	NavigationTab,
+	type NavigationTabType,
+	type Shapes,
+	type CapsuleGeometry,
+	type SphereGeometry,
+	type BoxGeometry,
+	type Geometry,
+	type Obstacle,
+	type Path,
+} from './types'
