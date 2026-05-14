599b8dc35f39c916ef5fcfec817985a75b3a01ae
Devin C <devin@viam.com>
2026-05-14 16:01:06 -0400

    lint

--- src/routes/widgets/card-list-item.svelte
+++ src/routes/widgets/card-list-item.svelte
@@ -37,11 +37,7 @@
 	import DoCommandWidget from '$lib/components/widgets/do-command/do-command.svelte'
 	import { getResourceAPI } from '$lib/get-resource-api'
 	import { getResourceKey } from '$lib/get-resource-key'
-	import {
-		type NamedResourceStatus,
-		ResourceStatusText,
-		widgetForResource,
-	} from '$lib/resource'
+	import { type NamedResourceStatus, ResourceStatusText, widgetForResource } from '$lib/resource'
 	import { scrollIntoView } from '$lib/scroll-into-view'
 
 	import ResourceStatus from './resource-status.svelte'
